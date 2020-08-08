41bf5c97a6c1da2c2ee246c2fd57acd6e7f3b296
andrewswint <andrewswint1976@gmail.com>
2020-08-07 21:59:45 -0500

    date diff not working

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,18 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+let date1 = new Date(document.getElementById("startDate").value);
+let date2 = new Date(document.getElementById("endDate").value);
 
-});
+function dateDiff(start, end) {
+    console.log(date1);
+    console.log(date2);
+    start = date1
+    end = date2
+    console.log(start);
+    console.log(end);
+    let timeDiff = Math.abs(end.getTime() - start.getTime());
+    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    console.log(start);
+    console.log(end);
+
+    document.getElementById("output").innerHTML = diffDays + " days";
+
+}
