dc8c3165382fdfb229808434ce5057717caebabf
andrewswint <andrewswint1976@gmail.com>
2020-08-16 20:07:16 -0500

    date difference working with string inputs

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,18 +1,14 @@
-let date1 = new Date(document.getElementById("startDate").value);
-let date2 = new Date(document.getElementById("endDate").value);
 
-function dateDiff(start, end) {
-    console.log(date1);
-    console.log(date2);
-    start = date1
-    end = date2
-    console.log(start);
-    console.log(end);
-    let timeDiff = Math.abs(end.getTime() - start.getTime());
-    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    console.log(start);
-    console.log(end);
-
-    document.getElementById("output").innerHTML = diffDays + " days";
-
+function dateDiff() {
+    let date1 = new Date(document.getElementById("startDate").value);
+    let date2 = new Date(document.getElementById("endDate").value);
+console.log("date1 " + date1);
+    const start = new Date(date1);
+    const end = new Date(date2);
+    console.log("start " + start);
+    const diffTime = Math.abs(end - start);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    console.log(diffTime + " milliseconds");
+    console.log(diffDays + " days");
+    document.getElementById("output").innerHTML=diffDays;
 }
